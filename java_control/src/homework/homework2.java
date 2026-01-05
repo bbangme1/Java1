f5293900db35b619da1206cb2889a3193419f18e
YJK <YJK@DESKTOP-F7LD0DN>
2026-01-05 21:31:03 +0900

    1월 5일

--- java_control/src/homework/homework2.java
+++ java_control/src/homework/homework2.java
@@ -9,25 +9,26 @@ public class homework2 {
 //		숫자 맞추기 입장 바꿔서
 
 		Scanner scan = new Scanner(System.in);
-
-		int UP = 1;
-		int DOWN = 2;
-		int Correct = 3;
-
+		
 		System.out.println("숫자를 입력하세요 : ");
 		int user = scan.nextInt();
 
 		while (true) {
 			int com = (int) Math.floor(Math.random() * 50) + 1;
-			System.out.println(com);
+			System.out.println("컴퓨터 숫자 : "+com);
 			System.out.println("UP 1 , DOWN 2 , CORRECT 3 입력 : ");
 			int user1 = scan.nextInt();
 			
-			if (com > user) {
-				System.out.println("UP 1 , DOWN 2 , CORRECT 3 입력 : ");
-
+			if (user1 == 1) {
+				int com1 = (int)Math.floor(Math.random()*50-com)+1;
 			}
-
+			else if (user1 == 2) {
+				int com1 = (int)Math.floor(Math.random()*50-com)-1;
+			}
+			else {
+				System.out.println("정답입니다");
+				break;
+			}
 		}
 
 	}
