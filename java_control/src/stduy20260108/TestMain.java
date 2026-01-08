5a82eb0fe837ae3b514aafeb0a32992f9f23e4e2
user <user@DESKTOP-8NBVUKG>
2026-01-08 10:21:56 +0900

    26.01.08

--- java_control/src/stduy20260108/TestMain.java
+++ java_control/src/stduy20260108/TestMain.java
@@ -9,11 +9,11 @@ public class TestMain {
 //		문제1. 10개의 무작위 정수 값 저장하기 ( 배열에 )
 //			   랜덤 범위는 1~50
 
-		int[] rendum = new int[10];
+		int[] random = new int[10];
 
-		for (int i = 0; i < rendum.length; i++) {
-			rendum[i] = (int) Math.floor(Math.random() * 50) + 1;
-			System.out.print(rendum[i] + " ");
+		for (int i = 0; i < random.length; i++) {
+			random[i] = (int) Math.floor(Math.random() * 50) + 1;
+			System.out.print(random[i] + " ");
 
 		}
 
