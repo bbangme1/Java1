21757d223d2e82ec14299a2f65a9bbd38590c607
YJK <YJK@DESKTOP-F7LD0DN>
2026-01-08 21:23:18 +0900

    OK

---
+++ test/src/test/MethodTest.java
@@ -0,0 +1,16 @@
+package test;
+
+public class MethodTest {
+
+	public static void main(String[] args) {
+		
+		Theater theanter = new Theater("바람","19세","5명");
+			
+		System.out.println(theanter.title);
+		System.out.println(theanter.price);
+		System.out.println(theanter.pesonnel);
+		
+
+	}
+
+}
---
+++ test/src/test/Theater.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Theater {
+	
+	String title;
+	String price;
+	String pesonnel;
+	
+	Theater(String title, String price, String pesonnel) {
+		this.title=title;
+		this.price=price;
+		this.pesonnel=pesonnel;
+	}
+
+}
