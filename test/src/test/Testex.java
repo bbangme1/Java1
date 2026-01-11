01f6501110e20b95f23e6eb2cd13927bdf743b7a
YJK <YJK@DESKTOP-F7LD0DN>
2026-01-11 12:44:11 +0900

    Merge branch 'main' of https://github.com/bbangme1/Java1.git into main

--- test/src/test/Testex.java
+++ test/src/test/Testex.java
@@ -9,8 +9,6 @@ public class Testex {
 		s.studentId = "2024001";
 
 		s.introduce();
-		s.study();
-		
-		
+		s.study();		
 	}
 }
--- test/src/test/Testex1.java
+++ test/src/test/Testex1.java
@@ -20,9 +20,7 @@ class Student extends Member {
 
 	@Override
 	public void introduce() {
-		System.out.println("학생 이름: " + name + ", 나이: " + age + " 학번 : " + studentId);
-		
-		
+		System.out.println("학생 이름: " + name + ", 나이: " + age + " 학번 : " + studentId);		
 	}
 }
 
