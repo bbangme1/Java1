6f124349fb3019680b438d304c322e4e91a60242
YJK <YJK@DESKTOP-F7LD0DN>
2026-01-11 12:40:31 +0900

    2026.01.11

---
+++ test/src/test/Testex.java
@@ -0,0 +1,14 @@
+package test;
+
+
+public class Testex {
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.name = "홍길동";
+		s.age = 20;
+		s.studentId = "2024001";
+
+		s.introduce();
+		s.study();
+	}
+}
---
+++ test/src/test/Testex1.java
@@ -0,0 +1,26 @@
+package test;
+
+class Member {
+
+	String name;
+	int age;
+
+	public void introduce() {
+		System.out.println("이름: " + name + ", 나이: " + age);
+	}
+}
+
+class Student extends Member {
+
+	String studentId;
+
+	public void study() {
+		System.out.println(name + "이(가) 공부를 합니다.");
+	}
+
+	@Override
+	public void introduce() {
+		System.out.println("학생 이름: " + name + ", 나이: " + age + " 학번 : " + studentId);
+	}
+}
+
