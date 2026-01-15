9aac93fb4cd976eb88682b98a0dd09fb96df63fd
YJK <YJK@DESKTOP-F7LD0DN>
2026-01-15 21:03:12 +0900

    26.1.15

---
+++ java_control/src/test/g.java
@@ -0,0 +1,74 @@
+package test;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class g {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println("가격을 입력하세요");
+//		int price = sc.nextInt();
+//		System.out.println("수량을 입력하세요");
+//		int quantity = sc.nextInt();
+//		
+//		int total = price * quantity;
+//		
+//		if(total >=50000)
+//			System.out.println("무료 배송입니다.");
+//		else if(total < 50000)
+//			System.out.println("배송비 3,000원이 추가됩니다.");
+
+//		for (int i = 0; i <= 30; i++) {
+//			if(i % 3==0)
+//				System.out.println("짝");
+//			else
+//			System.out.println(i);
+//		}
+
+//		int[] scores = new int[3];
+//
+//		for (int i = 0; i < scores.length; i++) {
+//			System.out.println((i+1)+"번 학생 점수를 입력하세요");
+//			scores[i] = sc.nextInt();
+//		}
+//		 double avg = calculateAverage(scores);
+//		 
+//		 System.out.println("평균 : "+avg);
+//		
+//	}
+//
+//	private static double calculateAverage(int[] arr) {
+//		int sum = 0;
+//		for (int score : arr) {
+//			sum += score;
+//		}
+//		return (double)sum/arr.length;
+		
+//		System.out.println("점수를 입력하세요");
+//		int j = sc.nextInt();
+//		
+//		if(j % 2 == 0)
+//			System.out.println("짝수");
+//		else
+//			System.out.println("홀수");
+		
+//		int n = sc.nextInt();
+//		
+//		for (int i = 1; i <= 9; i++) {
+//			int t = n*i;
+//			System.out.println(n+ "*"+ (i) + "=" + t);
+//		}
+		
+//		int n = sc.nextInt();
+//		
+//		for (int i = 0; i <= n; i++) {
+//			for (int j = 0; j < i; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+	}
+
+}
