be43b298fbc689f8dcc95039b47febe2e93e6960
YJK <YJK@DESKTOP-F7LD0DN>
2026-01-26 20:28:12 +0900

    26.1.26

---
+++ java_control/src/test/gg.java
@@ -0,0 +1,55 @@
+package test;
+
+import java.util.Scanner;
+
+public class gg {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+//		String hello = "Hello Java";
+//		System.out.println(hello);
+//
+//		System.out.println("정수를 입력하세요: ");
+//		int num = sc.nextInt();
+//		System.out.println("정수를 입력하세요: ");
+//		int num1 = sc.nextInt();
+//		System.out.println("합계 : "+(num + num1));
+
+//		int a = 2;
+//		int b = 7;
+//		System.out.println("a = "+a + "b = "+b);		
+//		int c = a;
+//		a = b;
+//		b = c;		
+//		System.out.println("a = "+a + "b = "+b);		
+
+//		if(num % 2 == 0) {
+//			System.out.println("짝");
+//		}else
+//			System.out.println("홀");
+
+//		int sum = 0;
+//		for (int i = 1; i < num; i++) {
+//			sum += i;
+//			System.out.println(sum);
+//		}
+
+//		int[] num = new int[]{5,10,15,20,25};
+//		int max = num[0];
+//		int min = num[0];
+//		
+//		for (int i = 0; i < num.length; i++) {
+//			if(num[i] > max) {
+//				max = num[i];
+//			}if(num[i] < min) {
+//				min = num[i];
+//			}
+//		}System.out.println("최대"+max);
+//		System.out.println("최소"+min);
+		
+		
+
+	}
+}
